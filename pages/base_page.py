a84180b1faa64d37671d280358d3aeb61a0925c1
hramina-ks <krist89@yandex.ru>
2022-08-24 02:12:17 +0500

    Добавила тест для login page и создала файлы для product page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,15 +1,15 @@
 from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=10): 
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
         
-    def open(self):
-        self.browser.get(self.url)
+    def open(self): # Открывает ссылку на страницу
+        self.browser.get(self.url) 
 
-    def is_element_present(self, how, what):
+    def is_element_present(self, how, what): # Проверяет наличие на странице элемента
         try:
             self.browser.find_element(how, what)
         except (NoSuchElementException):
--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,17 @@
-import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser,link) #Инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
